b888b02006a615732099885be4017d1a322b6d18
KabyleBOT <khaled.belkouche@gmail.com>
2024-02-12 17:55:12 +0100

    Update package version and modify attribute relationships in Product model

--- src/models/product.ts
+++ src/models/product.ts
@@ -1,18 +1,29 @@
-import { Entity, JoinTable, ManyToMany } from "typeorm";
+import {
+	Entity,
+	JoinTable,
+	ManyToMany,
+} from "typeorm";
 import { Product as MedusaProduct } from "@medusajs/medusa";
+import { Attribute } from "./attribute";
 import { AttributeValue } from "./attribute-value";
 import { IntAttributeValue } from "./int-attribute-value";
 
 @Entity()
 export class Product extends MedusaProduct {
-  @ManyToMany(() => AttributeValue)
-  @JoinTable()
-  attribute_values: AttributeValue[];
+	@ManyToMany(() => Attribute)
+	@JoinTable()
+	attributes: Attribute[];
 
-  @ManyToMany(() => IntAttributeValue, {
-    cascade: true,
-    onDelete: "CASCADE",
-  })
-  @JoinTable({ name: "int_attribute_values_products_product" })
-  int_attribute_values: IntAttributeValue[];
+	@ManyToMany(() => AttributeValue)
+	@JoinTable()
+	attribute_values: AttributeValue[];
+
+	@ManyToMany(() => IntAttributeValue, {
+		cascade: true,
+		onDelete: "CASCADE",
+	})
+	@JoinTable({
+		name: "int_attribute_values_products_product",
+	})
+	int_attribute_values: IntAttributeValue[];
 }
