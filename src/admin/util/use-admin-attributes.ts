6e28e618c176f6a8c1fd35ddffd46749c9f12271
KabyleBOT <khaled.belkouche@gmail.com>
2024-02-15 01:35:11 +0100

    Update package version and refactor useAdminAttributes function

--- src/admin/util/use-admin-attributes.ts
+++ src/admin/util/use-admin-attributes.ts
@@ -2,16 +2,27 @@ import { useQuery } from "@tanstack/react-query";
 import { $api } from "./api";
 import { Attribute } from "../../models/attribute";
 
-export const useAdminAttributes = (categories?: string[]) => {
-  const query = useQuery<Attribute[]>(["attributes", categories], async () => {
-    const response = await $api.get(`/admin/attributes`, {
-      params: {
-        categories,
-      },
-    });
+export const useAdminAttributes = (
+	categories?: string[]
+) => {
+	const query = useQuery<Attribute[]>(
+		["attributes", categories],
+		async () => {
+			const response = await $api.get(
+				`/admin/attributes`,
+				{
+					// params: {
+					//   categories,
+					// },
+				}
+			);
 
-    return response.data;
-  });
+			return response.data;
+		}
+	);
 
-  return { ...query, attributes: query.data || [] };
+	return {
+		...query,
+		attributes: query.data || [],
+	};
 };
