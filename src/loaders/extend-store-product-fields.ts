2ae5258e04f52f7722ed7754cf7121cb5677e0e1
KabyleBOT <khaled.belkouche@gmail.com>
2024-02-26 13:46:09 +0100

    Update package.json version and extend store product fields

--- src/loaders/extend-store-product-fields.ts
+++ src/loaders/extend-store-product-fields.ts
@@ -1,16 +1,17 @@
 export default async function () {
-  const imports = (await import(
-    "@medusajs/medusa/dist/api/routes/store/products/index"
-  )) as any;
-  imports.allowedStoreProductsRelations = [
-    ...imports.allowedStoreProductsRelations,
-    "custom_attributes",
-  ];
-  imports.defaultStoreProductsRelations = [
-    ...imports.defaultStoreProductsRelations,
-    "attribute_values",
-    "attribute_values.attribute",
-    "int_attribute_values",
-    "int_attribute_values.attribute",
-  ];
+	const imports = (await import(
+		"@medusajs/medusa/dist/api/routes/store/products/index"
+	)) as any;
+	imports.allowedStoreProductsFields = [
+		...imports.allowedStoreProductsFields,
+		"custom_attributes",
+	];
+	imports.allowedStoreProductsRelations =
+		[
+			...imports.allowedStoreProductsRelations,
+			"attribute_values",
+			"attribute_values.attribute",
+			"int_attribute_values",
+			"int_attribute_values.attribute",
+		];
 }
