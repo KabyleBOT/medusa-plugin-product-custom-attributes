9a19b4c94e40b480a61f26fe567c135d241d5d92
KabyleBOT <khaled.belkouche@gmail.com>
2024-02-26 14:04:31 +0100

    Update package.json and Product model to include custom_attributes

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,15 @@ import { AttributeValue } from "./models/attribute-value";
 import { IntAttributeValue } from "./models/int-attribute-value";
 
 declare module "@medusajs/medusa/dist/models/product" {
-  interface Product {
-    int_attribute_values?: IntAttributeValue[];
-    attribute_values?: AttributeValue[];
-    custom_attributes?: (Omit<Attribute, "beforeInsert"> & {
-      values?: AttributeValue[];
-      value?: IntAttributeValue;
-    })[];
-  }
+	interface Product {
+		int_attribute_values?: IntAttributeValue[];
+		attribute_values?: AttributeValue[];
+		custom_attributes?: CustomAttributeInProduct[];
+	}
 }
+
+export type CustomAttributeInProduct =
+	Omit<Attribute, "beforeInsert"> & {
+		values?: AttributeValue[];
+		value?: IntAttributeValue;
+	};
--- src/models/product.ts
+++ src/models/product.ts
@@ -1,18 +1,32 @@
-import { Entity, JoinTable, ManyToMany } from "typeorm";
+import {
+	Column,
+	Entity,
+	JoinTable,
+	ManyToMany,
+} from "typeorm";
 import { Product as MedusaProduct } from "@medusajs/medusa";
 import { AttributeValue } from "./attribute-value";
 import { IntAttributeValue } from "./int-attribute-value";
 
+import { CustomAttributeInProduct } from "..";
+
 @Entity()
 export class Product extends MedusaProduct {
-  @ManyToMany(() => AttributeValue)
-  @JoinTable()
-  attribute_values: AttributeValue[];
+	@ManyToMany(() => AttributeValue)
+	@JoinTable()
+	attribute_values: AttributeValue[];
 
-  @ManyToMany(() => IntAttributeValue, {
-    cascade: true,
-    onDelete: "CASCADE",
-  })
-  @JoinTable({ name: "int_attribute_values_products_product" })
-  int_attribute_values: IntAttributeValue[];
+	@ManyToMany(() => IntAttributeValue, {
+		cascade: true,
+		onDelete: "CASCADE",
+	})
+	@JoinTable({
+		name: "int_attribute_values_products_product",
+	})
+	int_attribute_values: IntAttributeValue[];
+
+	@Column({
+		nullable: true,
+	})
+	custom_attributes: CustomAttributeInProduct[];
 }
