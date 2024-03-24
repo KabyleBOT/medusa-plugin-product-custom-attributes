94580b20913c88565379477e68be6031bdc0b7f0
KabyleBOT <khaled.belkouche@gmail.com>
2024-03-25 00:44:29 +0100

    Update package.json version and extend admin product fields

--- src/loaders/extend-admin-product-fields.ts
+++ src/loaders/extend-admin-product-fields.ts
@@ -1,23 +1,29 @@
-export default async function (container) {
-  const imports = (await import(
-    "@medusajs/medusa/dist/api/routes/admin/products/index"
-  )) as any;
-  imports.defaultAdminProductRelations = [
-    ...imports.defaultAdminProductRelations,
-    "attribute_values",
-    "attribute_values.attribute",
-    "int_attribute_values",
-    "int_attribute_values.attribute",
-  ];
-  imports.defaultAdminProductRemoteQueryObject = {
-    ...imports.defaultAdminProductRemoteQueryObject,
-    attribute_values: {
-      relation: "attribute_values",
-      fields: ["value", "metadata"],
-    },
-    int_attribute_values: {
-      relation: "int_attribute_values",
-      fields: ["value"],
-    },
-  };
+export default async function (
+	container
+) {
+	const imports = (await import(
+		"@medusajs/medusa/dist/api/routes/admin/products/index"
+	)) as any;
+	imports.defaultAdminProductRelations =
+		[
+			...imports.defaultAdminProductRelations,
+			"custom_attributes",
+			"attribute_values",
+			"attribute_values.attribute",
+			"int_attribute_values",
+			"int_attribute_values.attribute",
+		];
+	imports.defaultAdminProductRemoteQueryObject =
+		{
+			...imports.defaultAdminProductRemoteQueryObject,
+			attribute_values: {
+				relation: "attribute_values",
+				fields: ["value", "metadata"],
+			},
+			int_attribute_values: {
+				relation:
+					"int_attribute_values",
+				fields: ["value"],
+			},
+		};
 }
