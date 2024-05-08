ec462d1ca940c5b32ba7e017309ece032aa5e5ac
KabyleBOT <khaled.belkouche@gmail.com>
2024-05-08 12:32:46 +0200

    chore: Update package version and  commente code in extend-store-product-fields.ts

--- src/loaders/extend-store-product-fields.ts
+++ src/loaders/extend-store-product-fields.ts
@@ -1,20 +1,20 @@
-export default async function () {
-	const imports = (await import(
-		"@medusajs/medusa/dist/api/routes/store/products/index"
-	)) as any;
-	imports.allowedStoreProductsRelations =
-		[
-			...imports.allowedStoreProductsRelations,
-			"attribute_values",
-			"attribute_values.attribute",
-			"int_attribute_values",
-			"int_attribute_values.attribute",
-			"custom_attributes",
-			"custom_attributes.values",
-			"custom_attributes.int_values",
-		];
-	imports.defaultStoreProductsRelations =
-		[
-			...imports.defaultStoreProductsRelations,
-		];
-}
+// export default async function () {
+// 	const imports = (await import(
+// 		"@medusajs/medusa/dist/api/routes/store/products/index"
+// 	)) as any;
+// 	imports.allowedStoreProductsRelations =
+// 		[
+// 			...imports.allowedStoreProductsRelations,
+// 			"attribute_values",
+// 			"attribute_values.attribute",
+// 			"int_attribute_values",
+// 			"int_attribute_values.attribute",
+// 			"custom_attributes",
+// 			"custom_attributes.values",
+// 			"custom_attributes.int_values",
+// 		];
+// 	imports.defaultStoreProductsRelations =
+// 		[
+// 			...imports.defaultStoreProductsRelations,
+// 		];
+// }
