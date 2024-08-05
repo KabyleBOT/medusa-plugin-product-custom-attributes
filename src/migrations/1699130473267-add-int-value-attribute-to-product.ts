1e30181994014045d5427282857277c163fff405
KabyleBOT <khaled.belkouche@gmail.com>
2024-08-05 17:12:54 +0200

    correct migration and repo extending

--- src/migrations/1699130473267-add-int-value-attribute-to-product.ts
+++ src/migrations/1699130473267-add-int-value-attribute-to-product.ts
@@ -1,17 +1,22 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import {
+	MigrationInterface,
+	QueryRunner,
+} from "typeorm";
 
 export class AddIntValueAttributeToProduct1699130473267
-  implements MigrationInterface
+	implements MigrationInterface
 {
-  public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`
+	public async up(
+		queryRunner: QueryRunner
+	): Promise<void> {
+		await queryRunner.query(`
             CREATE TABLE "int_attribute_values_products_product" (
                 "intAttributeValueId" character varying,
                 "productId" character varying
             );
         `);
 
-    await queryRunner.query(`
+		await queryRunner.query(`
             ALTER TABLE "int_attribute_values_products_product"
             ADD CONSTRAINT "FK_int_attribute_values_products_product_int_attribute_value" 
             FOREIGN KEY ("intAttributeValueId") 
@@ -19,28 +24,30 @@ export class AddIntValueAttributeToProduct1699130473267
             ON DELETE CASCADE;
         `);
 
-    await queryRunner.query(`
+		await queryRunner.query(`
             ALTER TABLE "int_attribute_values_products_product"
             ADD CONSTRAINT "FK_int_attribute_values_products_product_product" 
             FOREIGN KEY ("productId") 
             REFERENCES "product"("id") 
             ON DELETE CASCADE;
         `);
-  }
+	}
 
-  public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`
-            DROP TABLE "int_attribute_values_products_product";
-        `);
-
-    await queryRunner.query(`
+	public async down(
+		queryRunner: QueryRunner
+	): Promise<void> {
+		await queryRunner.query(`
             ALTER TABLE "int_attribute_values_products_product"
             DROP CONSTRAINT "FK_int_attribute_values_products_product_int_attribute_value";
         `);
 
-    await queryRunner.query(`
+		await queryRunner.query(`
             ALTER TABLE "int_attribute_values_products_product"
             DROP CONSTRAINT "FK_int_attribute_values_products_product_product";
         `);
-  }
+
+		await queryRunner.query(`
+            DROP TABLE "int_attribute_values_products_product";
+        `);
+	}
 }
