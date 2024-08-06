f13fc7f4c72b92a2630d9d12b1f458e332cf930e
KabyleBOT <khaled.belkouche@gmail.com>
2024-08-06 12:44:51 +0200

    chore: Update package version and extend admin product fields

--- src/repositories/product.ts
+++ src/repositories/product.ts
@@ -2,14 +2,14 @@ import { dataSource } from "@medusajs/medusa/dist/loaders/database";
 import { ProductRepository as MedusaProductRepository } from "@medusajs/medusa/dist/repositories/product";
 import { Product } from "../models/product";
 
-// Create a new repository instance for your custom Product model
 export const ProductRepository =
-	dataSource.getRepository(Product);
-
-// Set the prototype of your custom repository to Medusa's ProductRepository
-Object.setPrototypeOf(
-	ProductRepository,
-	MedusaProductRepository.prototype
-);
+	dataSource
+		.getRepository(Product)
+		.extend(
+			Object.assign(
+				MedusaProductRepository,
+				{ target: Product }
+			)
+		);
 
 export default ProductRepository;
