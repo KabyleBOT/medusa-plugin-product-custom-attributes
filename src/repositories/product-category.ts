eb1f2be6c3adb03ab4d61c643e7b552b9e00c8f0
KabyleBOT <khaled.belkouche@gmail.com>
2024-08-06 17:01:55 +0200

    chore: Bump package version to 1.3.10 and update product repository

--- src/repositories/product-category.ts
+++ src/repositories/product-category.ts
@@ -1,15 +1,17 @@
-import { ProductCategory } from "../models/product-category";
 import { dataSource } from "@medusajs/medusa/dist/loaders/database";
 import { ProductCategoryRepository as MedusaProductCategoryRepository } from "@medusajs/medusa/dist/repositories/product-category";
+import { ProductCategory } from "../models/product-category";
+
+// see https://github.com/medusajs/medusa/issues/6139
 
 export const ProductCategoryRepository =
 	dataSource
 		.getTreeRepository(ProductCategory)
-		.extend({
-			...Object.assign(
+		.extend(
+			Object.assign(
 				MedusaProductCategoryRepository,
 				{ target: ProductCategory }
-			),
-		});
+			)
+		);
 
 export default ProductCategoryRepository;
