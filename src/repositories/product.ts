ea93b220b6552de214d3e091a1379c9a7db6dd00
KabyleBOT <khaled.belkouche@gmail.com>
2024-08-06 12:35:10 +0200

    chore: Update package version and refactor attribute service

--- src/repositories/product.ts
+++ src/repositories/product.ts
@@ -2,14 +2,14 @@ import { dataSource } from "@medusajs/medusa/dist/loaders/database";
 import { ProductRepository as MedusaProductRepository } from "@medusajs/medusa/dist/repositories/product";
 import { Product } from "../models/product";
 
+// Create a new repository instance for your custom Product model
 export const ProductRepository =
-	dataSource
-		.getRepository(Product)
-		.extend({
-			...Object.assign(
-				MedusaProductRepository,
-				{ target: Product }
-			),
-		});
+	dataSource.getRepository(Product);
+
+// Set the prototype of your custom repository to Medusa's ProductRepository
+Object.setPrototypeOf(
+	ProductRepository,
+	MedusaProductRepository.prototype
+);
 
 export default ProductRepository;
