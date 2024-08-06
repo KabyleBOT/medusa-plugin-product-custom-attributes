fa3b46cf903d0e9fe23a4d94e775904a572048e3
KabyleBOT <khaled.belkouche@gmail.com>
2024-08-06 14:28:44 +0200

    chore: Bump package version to 1.3.8 and import MedusaProduct in product repository

--- src/repositories/product.ts
+++ src/repositories/product.ts
@@ -1,3 +1,4 @@
+import { Product as MedusaProduct } from "@medusajs/medusa";
 import { dataSource } from "@medusajs/medusa/dist/loaders/database";
 import {
 	// alias the core repository to not cause a naming conflict
@@ -8,20 +9,13 @@ import { Product } from "../models/product";
 export const ProductRepository =
 	dataSource
 		.getRepository(Product)
-		.extend({
-			// it is important to spread the existing repository here.
-			//  Otherwise you will end up losing core properties
-			...Object.assign(
+		.extend(
+			Object.assign(
 				MedusaProductRepository,
 				{
-					target: Product,
+					target: MedusaProduct,
 				}
-			),
-
-			/**
-			 * Here you can create your custom function
-			 * For example
-			 */
-		});
+			)
+		);
 
 export default ProductRepository;
