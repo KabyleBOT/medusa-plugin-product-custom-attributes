2921c4b12f0673442148f6e0099059bc8efdacaf
KabyleBOT <khaled.belkouche@gmail.com>
2024-08-06 13:28:26 +0200

    chore: Bump package version to 1.3.5 and extend ProductRepository

--- src/repositories/product.ts
+++ src/repositories/product.ts
@@ -19,11 +19,16 @@ import { dataSource } from "@medusajs/medusa/dist/loaders/database";
 import { ProductFilterOptions } from "@medusajs/medusa/dist/types/product";
 import { Product } from "../models/product";
 import { ProductCategory } from "../models/product-category";
+import { ProductRepository as MedusaProductRepository } from "@medusajs/medusa/dist/repositories/product";
 
 export const ProductRepository =
 	dataSource
 		.getRepository(Product)
 		.extend({
+			...Object.assign(
+				MedusaProductRepository,
+				{ target: Product }
+			),
 			async bulkAddToCollection(
 				productIds: string[],
 				collectionId: string
